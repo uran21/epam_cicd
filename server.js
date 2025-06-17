c88ab5947d6d7b9c1005de4736257b2ab568a7ca
uran <uran21@mail.ru>
2025-06-17 10:05:23 +0000

    test

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 const express = require('express');
 
-// Constants
-const PORT = 3000;
+// Get port from environment, default to 3000 if not provided
+const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
 // App
@@ -12,5 +12,6 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
